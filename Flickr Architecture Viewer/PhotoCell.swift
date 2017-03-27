0bca3ba5e6e73d26e953a7af133b4f3ec3b78cbe
Lopastvertoleta <tparkhomenko@icloud.com>
2017-03-27 23:48:50 +0700

    - Basic UI

---
+++ Flickr Architecture Viewer/PhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCell.swift
+//  Flickr Architecture Viewer
+//
+//  Created by Taras Parkhomenko on 27/03/2017.
+//  Copyright © 2017 Taras Parkhomenko. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+}
--- Flickr Architecture Viewer/PhotosCollectionViewController.swift
+++ Flickr Architecture Viewer/PhotosCollectionViewController.swift
@@ -7,88 +7,54 @@
 //
 
 import UIKit
+import SDWebImage
+import Realm
+import RealmSwift
 
-private let reuseIdentifier = "Cell"
+private let reuseIdentifier = "PhotoCell"
 
-class PhotosCollectionViewController: UICollectionViewController {
-
+class PhotosCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    var notificationToken:NotificationToken?
+    var photos:Results<Photo>?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
+        PhotosNetworking.shared.fetchImages()
+        photos = try? Realm().objects(Photo.self).sorted(byKeyPath: "added")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        notificationToken = photos?.addNotificationBlock({ [weak self] (_) in
+            self?.collectionView?.reloadData()
+        })
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        notificationToken?.stop()
     }
-    */
 
     // MARK: UICollectionViewDataSource
-
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-        return 0
+        guard let photos = photos else { return 0 }
+        return photos.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
-        // Configure the cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! PhotoCell
+        
+        let photo = photos![indexPath.row]
+        cell.photoImageView.sd_setImage(with: URL(string: photo.thumbURL)!, placeholderImage: #imageLiteral(resourceName: "Placeholder"))
     
         return cell
     }
-
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let dimension = UIScreen.main.bounds.width / 2 - 0.5
+        return CGSize(width: dimension, height: dimension)
+    }
+    
     // MARK: UICollectionViewDelegate
 
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
 
 }
