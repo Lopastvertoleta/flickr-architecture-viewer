c642b92bebc0dc0c986acd7a07230fd5dbbd0954
Lopastvertoleta <tparkhomenko@icloud.com>
2017-03-28 20:52:08 +0700

    - Expand image on select

--- Flickr Architecture Viewer/PhotosCollectionViewController.swift
+++ Flickr Architecture Viewer/PhotosCollectionViewController.swift
@@ -17,6 +17,7 @@ class PhotosCollectionViewController: UICollectionViewController, UICollectionVi
     
     var notificationToken:NotificationToken?
     var photos:Results<Photo>?
+    var expanded = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,18 +44,37 @@ class PhotosCollectionViewController: UICollectionViewController, UICollectionVi
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! PhotoCell
         
+        cell.photoImageView.contentMode = expanded ? .scaleAspectFit : .scaleAspectFill
+        
         let photo = photos![indexPath.row]
-        cell.photoImageView.sd_setImage(with: URL(string: photo.thumbURL)!, placeholderImage: #imageLiteral(resourceName: "Placeholder"))
-    
+        cell.photoImageView.sd_setImage(with: URL(string: expanded ? photo.url : photo.thumbURL)!, placeholderImage: #imageLiteral(resourceName: "Placeholder"))
+        
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let dimension = UIScreen.main.bounds.width / 2 - 0.5
+        if (expanded) { return collectionView.bounds.size }
         return CGSize(width: dimension, height: dimension)
     }
     
     // MARK: UICollectionViewDelegate
 
-
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if (self.expanded) {
+            self.expanded = false
+            (collectionView.collectionViewLayout as? UICollectionViewFlowLayout)?.scrollDirection = .vertical
+            collectionView.isPagingEnabled = false
+            collectionView.layer.backgroundColor = UIColor.white.cgColor
+            collectionView.scrollToItem(at: indexPath, at: .top, animated: false)
+        } else {
+            self.expanded = true
+            (collectionView.collectionViewLayout as? UICollectionViewFlowLayout)?.scrollDirection = .horizontal
+            collectionView.isPagingEnabled = true
+            collectionView.layer.backgroundColor = UIColor.black.cgColor
+            
+            collectionView.scrollToItem(at: indexPath, at: .right, animated: false)
+        }
+        collectionView.reloadData()
+    }
 }
